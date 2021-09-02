5a975b403878761d80a33d492fb3b5f789d8f5f9
Sadman95 <mail.sakib.1995@gmail.com>
2021-09-02 18:35:42 +0600

    some validation added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
+// error handler
+const errorText = document.getElementById('error-text');
+
 /* Search button onclick function */
+const totalSearch = document.getElementById('total-search');
 const searchField = document.getElementById('search-field');
 const loadBooks = async () => {
-    const url = `https://openlibrary.org/search.json?q=${searchField.value}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    displayBooksInfo(data.docs);
-    searchField.value = '';
+    if(searchField.value === ''){
+        errorText.innerText = 'Pleasse search something'
+        totalSearch.classList.add('d-none')
+    }
+
+    else{
+        totalSearch.classList.remove('d-none')
+        errorText.classList.add('d-none')
+        const url = `https://openlibrary.org/search.json?q=${searchField.value}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        displayBooksInfo(data.docs);
+        searchField.value = '';
+        details.textContent = '';
+    }
 }
 
 /* Data load to site */
@@ -13,15 +27,14 @@ const displayBooksInfo = details => {
     const booksGallery = document.getElementById('books-gallery');
     booksGallery.textContent = '';
     /* total search results */
-    const totalSearch = document.getElementById('total-search');
     totalSearch.innerText = `${details.length} results found`;
     details.forEach(detail => {
-        // console.log(detail);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-            <div class="card h-100">
-                <img src= "https://covers.openlibrary.org/b/id/${detail.cover_i}-M.jpg" class="card-img-top" alt="...">
+            <div onclick="loadDetails('${detail.cover_i}')" 
+            class="card h-100">
+                <img src= "https://covers.openlibrary.org/b/id/${detail.cover_i ? detail.cover_i : '<a href="#">No image</a>'}-M.jpg" class="card-img-top">
                 <div class="card-body">
                     <h5 class="card-title">${detail.title}</h5>
                     1st published year: <p>${detail.first_publish_year}</p> 
@@ -37,8 +50,12 @@ const displayBooksInfo = details => {
     );
 }
 
-
-
-
+/* Single book detail */
+const details = document.getElementById('details');
+const loadDetails = (info) =>{
+    details.innerHTML = `
+        <img src="https://covers.openlibrary.org/b/id/${info}-M.jpg" class="card-img-top" alt="...">
+    `;
+}
 
 
