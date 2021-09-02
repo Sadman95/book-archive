1d31be45bc265e52e711b83209bc8f794599d612
Sadman95 <mail.sakib.1995@gmail.com>
2021-09-02 08:32:53 +0600

    total search results added

--- js/app.js
+++ js/app.js
@@ -5,14 +5,15 @@ const loadBooks = async () => {
     const res = await fetch(url);
     const data = await res.json();
     displayBooksInfo(data.docs);
+    searchField.value = '';
 }
 
 /* Data load to site */
 const booksGallery = document.getElementById('books-gallery');
 const displayBooksInfo = details => {
-    console.log(details.length);                    /* total book of single catagory */
+    const totalSearch = document.getElementById('total-search');
+    totalSearch.innerText = `${details.length} results found`;
     details.forEach(detail => {
-        const publishers = detail.publisher;
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -21,14 +22,16 @@ const displayBooksInfo = details => {
                 <div class="card-body">
                     <h5 class="card-title">${detail.title}</h5>
                     1st published year: <p>${detail.first_publish_year}</p> 
-                    <p class="card-text">Publisher: ${publishers}</p>
+                    <p class="card-text">Publisher: ${detail.publisher}</p>
                 </div>
                 <div class="card-footer">
                     <a href="#" class="text-muted">By: ${detail.author_name}</a>
                 </div>
             </div>
         `;
-        booksGallery.appendChild(div);   
-    })
+        booksGallery.appendChild(div);
+    }
+    );       
 }
+
 
