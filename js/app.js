c0359820e81af8c6a6e402ea12ec4a519cc475c8
Sadman95 <mail.sakib.1995@gmail.com>
2021-09-02 20:03:06 +0600

    dynamic done

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
 // error handler
-const errorText = document.getElementById('error-text');
+const errorMsg = meassage => {
+    const errorText = document.getElementById('error-text');
+    errorText.innerText = meassage;
+    totalSearch.classList.add('d-none');
+}
+
 
 /* Search button onclick function */
 const totalSearch = document.getElementById('total-search');
 const searchField = document.getElementById('search-field');
 const loadBooks = async () => {
     if(searchField.value === ''){
-        errorText.innerText = 'Pleasse search something'
-        totalSearch.classList.add('d-none')
+        errorMsg('please search for books');
     }
 
     else{
-        totalSearch.classList.remove('d-none')
-        errorText.classList.add('d-none')
+        errorMsg('');
         const url = `https://openlibrary.org/search.json?q=${searchField.value}`;
         const res = await fetch(url);
         const data = await res.json();
@@ -24,38 +27,47 @@ const loadBooks = async () => {
 
 /* Data load to site */
 const displayBooksInfo = details => {
-    const booksGallery = document.getElementById('books-gallery');
-    booksGallery.textContent = '';
-    /* total search results */
-    totalSearch.innerText = `${details.length} results found`;
-    details.forEach(detail => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-            <div onclick="loadDetails('${detail.cover_i}')" 
-            class="card h-100">
-                <img src= "https://covers.openlibrary.org/b/id/${detail.cover_i ? detail.cover_i : '<a href="#">No image</a>'}-M.jpg" class="card-img-top">
-                <div class="card-body">
-                    <h5 class="card-title">${detail.title}</h5>
-                    1st published year: <p>${detail.first_publish_year}</p> 
-                    <p class="card-text">Publisher: ${detail.publisher}</p>
-                </div>
-                <div class="card-footer">
-                    <a href="#" class="text-muted">By: ${detail.author_name}</a>
-                </div>
-            </div>
-        `;
-        booksGallery.appendChild(div);
+    if(details.length === 0){
+        errorMsg('Nothing to Display');
     }
-    );
+    else{
+        const booksGallery = document.getElementById('books-gallery');
+        booksGallery.textContent = '';
+        /* total search results */
+        totalSearch.classList.remove('d-none');
+        totalSearch.innerText = `${details.length} results found`;
+        details.forEach(detail => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                <div onclick="loadDetails('${detail.cover_i}', details)" 
+                class="card h-100">
+                    <img src= "https://covers.openlibrary.org/b/id/${detail.cover_i ? detail.cover_i : '<a href="#">No image</a>'}-M.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">${detail.title}</h5>
+                        1st published year: <p>${detail.first_publish_year}</p> 
+                        <p class="card-text">Publisher: ${detail.publisher}</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="text-muted">By: ${detail.author_name}</a>
+                    </div>
+                </div>
+            `;
+            booksGallery.appendChild(div);
+        }
+        );
+    }
+
 }
 
 /* Single book detail */
 const details = document.getElementById('details');
-const loadDetails = (info) =>{
+details.classList.add('d-none');
+const loadDetails = (info,displayDetail) =>{
     details.innerHTML = `
         <img src="https://covers.openlibrary.org/b/id/${info}-M.jpg" class="card-img-top" alt="...">
     `;
+    displayDetail.classList.remove('d-none');
 }
 
 
